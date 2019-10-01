05466e95983cfb36bb60346a53df9e17fd6ccfdc
Landon Taylor <landon.autumn.taylor@gmail.com>
2019-10-01 12:41:47 -0500

    color

---
+++ public/precache-manifest.6d549e351e82894263970532c94d67a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e344759daa317ce27b00d1e6d4b9e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ee76e51708281f176a8",
+    "url": "/static/css/main.578421a2.chunk.css"
+  },
+  {
+    "revision": "83134ed4d27a30826b49",
+    "url": "/static/js/2.fbc0b32f.chunk.js"
+  },
+  {
+    "revision": "9ee76e51708281f176a8",
+    "url": "/static/js/main.cf2226f2.chunk.js"
+  },
+  {
+    "revision": "0385eca952409dcf9bae",
+    "url": "/static/js/runtime-main.748c9e13.js"
+  },
+  {
+    "revision": "1944bd802d389e0fcbf3b229322f9744",
+    "url": "/static/media/glasses.1944bd80.png"
+  },
+  {
+    "revision": "a5b64575d4af2928ddbca83302d0acd5",
+    "url": "/static/media/midnight.a5b64575.gif"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bd9bbcc49857f90f1c3aa33a871a202.js"
+  "/precache-manifest.6d549e351e82894263970532c94d67a0.js"
 );
 
 self.addEventListener('message', (event) => {
